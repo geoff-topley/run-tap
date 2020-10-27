f15286fc90bdf2c04c3ac2fd8877144adc5b825c
Geoff Topley <geoff.topley@gmail.com>
2020-10-27 13:26:09 +0000

    Convert laps and home components to functions

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,25 +3,23 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import runTaplogo from "../../../src/runTaplogo.png";
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div>
-        <Row style={{ marginTop: "16px" }}>
-          <Col>
-            <img
-              src={runTaplogo}
-              alt="runTaplogo"
-              style={{
-                maxHeight: "100%",
-                maxWidth: "100%",
-              }}
-            />
-          </Col>
-        </Row>
-      </div>
-    );
-  }
-}
+const Home = () => {
+  return (
+    <div>
+      <Row style={{ marginTop: "16px" }}>
+        <Col>
+          <img
+            src={runTaplogo}
+            alt="runTaplogo"
+            style={{
+              maxHeight: "100%",
+              maxWidth: "100%",
+            }}
+          />
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
 export default Home;
