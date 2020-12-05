d572e8a26c76f7c953001c5a6e7aa43f2c83257d
Geoff Topley <geoff.topley@gmail.com>
2020-12-05 14:01:09 +0000

    Update NavDropdown to show/hide on hover

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import stravaConnect from "../../../src/stravaConnect.png";
 import { NavLink } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
@@ -11,57 +11,74 @@ function redirect() {
   window.location.href = `https://www.strava.com/oauth/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${process.env.REACT_APP_REDIRECT_URL}/exchange_token&approval_prompt=force&scope=activity:read,activity:write,profile:read_all`;
 }
 
-const Navigation = (props) => (
-  <Navbar bg="light" expand="lg">
-    <Navbar.Brand as={NavLink} to="/" exact>
-      Run-Tap
-    </Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mr-auto">
-        <Nav.Link as={NavLink} to="/" exact>
-          Home
-        </Nav.Link>
+const Navigation = (props) => {
+  const [show, setShow] = useState(false);
 
-        {props.auth.isAuthenticated() ? (
-          <NavDropdown title="Activities" id="basic-nav-dropdown">
-            <NavDropdown.Item as={NavLink} to="/activities" exact>
-              Recent
-            </NavDropdown.Item>
-            <NavDropdown.Item as={NavLink} to="/races">
-              Races
-            </NavDropdown.Item>
-            {/* <NavDropdown.Divider />
-            <NavDropdown.Item href="#tbd">Search All</NavDropdown.Item> */}
-          </NavDropdown>
-        ) : null}
+  const showDropdown = () => {
+    setShow(!show);
+  };
+  const hideDropdown = () => {
+    setShow(false);
+  };
 
-        {props.auth.isAuthenticated() ? (
-          <Nav.Link as={NavLink} to="/profile" exact>
-            Profile
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand as={NavLink} to="/" exact>
+        Run-Tap
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link as={NavLink} to="/" exact>
+            Home
           </Nav.Link>
-        ) : null}
 
-        <Nav.Link as={NavLink} to="/about" exact>
-          About
-        </Nav.Link>
-      </Nav>
-      <Form inline>
-        {props.auth.isAuthenticated() ? (
-          <Button variant="light" onClick={props.auth.logout}>
-            Logout
-          </Button>
-        ) : (
-          <img
-            src={stravaConnect}
-            alt="stravaConnect"
-            style={{ cursor: "pointer" }}
-            onClick={redirect}
-          />
-        )}
-      </Form>
-    </Navbar.Collapse>
-  </Navbar>
-);
+          {props.auth.isAuthenticated() ? (
+            <NavDropdown
+              title="Activities"
+              id="basic-nav-dropdown"
+              show={show}
+              onMouseEnter={showDropdown}
+              onMouseLeave={hideDropdown}
+            >
+              <NavDropdown.Item as={NavLink} to="/activities" exact>
+                Recent
+              </NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/races">
+                Races
+              </NavDropdown.Item>
+              {/* <NavDropdown.Divider />
+            <NavDropdown.Item href="#tbd">Search All</NavDropdown.Item> */}
+            </NavDropdown>
+          ) : null}
+
+          {props.auth.isAuthenticated() ? (
+            <Nav.Link as={NavLink} to="/profile" exact>
+              Profile
+            </Nav.Link>
+          ) : null}
+
+          <Nav.Link as={NavLink} to="/about" exact>
+            About
+          </Nav.Link>
+        </Nav>
+        <Form inline>
+          {props.auth.isAuthenticated() ? (
+            <Button variant="light" onClick={props.auth.logout}>
+              Logout
+            </Button>
+          ) : (
+            <img
+              src={stravaConnect}
+              alt="stravaConnect"
+              style={{ cursor: "pointer" }}
+              onClick={redirect}
+            />
+          )}
+        </Form>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 
 export default Navigation;
