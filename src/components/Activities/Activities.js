177e8c84ba3b0036e075e9a275c5ed06b5f08a8e
Geoff Topley <geoff.topley@gmail.com>
2021-01-23 13:08:42 +0000

    Convert Activities to functional component and use hooks

--- src/components/Activities/Activities.js
+++ src/components/Activities/Activities.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Loader from "../Loader/Loader";
 import Activity from "../ActivityCard/ActivityCard";
 import Row from "react-bootstrap/Row";
@@ -9,31 +9,28 @@ import Button from "react-bootstrap/Button";
 import { handleError } from "../../errorHandling/ErrorHandling";
 import * as calculate from "../../helpers/calculations";
 
-class Activities extends React.Component {
-  constructor(props) {
-    super(props);
-    this.stravaInstance = this.props.auth.setStravaInstance();
-  }
+const Activities = ({ auth }) => {
+  const stravaInstance = auth.setStravaInstance();
 
-  state = {
-    isPageLoading: true,
-    searchCriteria: "",
-    activities: [],
-    activitiesFound: [],
-  };
+  const [pageLoading, setPageLoading] = useState(true);
+  const [searchCriteria, setSearchCriteria] = useState("");
+  const [activities, setActivities] = useState([]);
+  const [activitiesFound, setActivitiesFound] = useState([]);
 
-  componentDidMount() {
-    this.loadActivities();
-  }
+  // second argument - a list of reasons useEffect should run (dependency array). [] means run once
+  useEffect(() => {
+    loadActivities();
+  }, []);
 
-  loadActivities = () => {
-    const url = `/athlete/activities?per_page=200`;
+  const loadActivities = () => {
+    const url = `/athlete/activities?per_page=48`;
 
-    this.stravaInstance
+    stravaInstance
       .get(url)
       .then((response) => {
         let activities = response.data;
-        this.setState({ activities, isPageLoading: false });
+        setActivities(activities);
+        setPageLoading(false);
       })
       .catch((error) => {
         console.log(error);
@@ -43,17 +40,16 @@ class Activities extends React.Component {
           "3000",
           "error"
         );
-        this.setState({ isPageLoading: false });
+        setPageLoading(false);
       });
   };
 
-  changeActivitySearchValue = (event) => {
+  const changeActivitySearchValue = (event) => {
     let searchCriteria = event.target.value;
-    this.setState({ searchCriteria });
+    setSearchCriteria(searchCriteria);
   };
 
-  searchActivities = () => {
-    const { searchCriteria, activities } = this.state;
+  const searchActivities = () => {
     let activitiesFound = activities.filter((activity) => {
       if (
         activity.name.toLowerCase().indexOf(searchCriteria.toLowerCase()) !== -1
@@ -69,92 +65,79 @@ class Activities extends React.Component {
         "2000",
         "warning"
       );
-      this.setState((prevState) => ({
-        activitiesFound: prevState.activitiesFound,
-      }));
     } else {
-      this.setState({ activitiesFound });
+      setActivitiesFound(activitiesFound);
     }
   };
 
-  clearSearch = () => {
-    this.setState({
-      searchCriteria: "",
-      activitiesFound: [],
-    });
+  const clearSearch = () => {
+    setSearchCriteria("");
+    setActivitiesFound([]);
   };
 
-  render() {
-    const { activities, activitiesFound } = this.state;
-    const numOfColumns = 3;
+  const numOfColumns = 3;
+  const activityList =
+    activitiesFound.length > 0 ? activitiesFound : activities;
 
-    const activityList =
-      activitiesFound.length > 0 ? activitiesFound : activities;
-
-    const raceGrid = activityList
-      .map((_activity, index) => {
-        return index % numOfColumns === 0
-          ? activityList.slice(index, index + numOfColumns)
-          : null;
-      })
-      .filter((activity) => activity)
-      .map((row, index) => {
-        return (
-          <Row key={index}>
-            {row.map((rowItem) => {
-              return (
-                <Col sm={4} style={{ padding: "16px" }} key={rowItem.id}>
-                  <Activity
-                    name={rowItem.name}
-                    workoutType={rowItem.workout_type}
-                    startDate={rowItem.start_date}
-                    distance={calculate.metersToMiles(rowItem.distance, 2)}
-                    time={calculate.secondsToMinutes(rowItem.moving_time)}
-                    id={rowItem.id}
-                    routeToFullActivity={this.routeToFullActivity}
-                  />
-                </Col>
-              );
-            })}
-          </Row>
-        );
-      });
-
-    return (
-      <>
-        <Row>
-          <Col sm={4} style={{ padding: "16px" }}>
-            <InputGroup>
-              <FormControl
-                placeholder="Search"
-                aria-label="Search"
-                aria-describedby="basic-addon2"
-                onChange={this.changeActivitySearchValue}
-                onKeyPress={(e) => {
-                  if (e.key === "Enter") {
-                    this.searchActivities();
-                  }
-                }}
-                value={this.state.searchCriteria}
-              />
-              <InputGroup.Append>
-                <Button
-                  variant="outline-secondary"
-                  onClick={this.searchActivities}
-                >
-                  Search
-                </Button>
-                <Button variant="outline-secondary" onClick={this.clearSearch}>
-                  Clear
-                </Button>
-              </InputGroup.Append>
-            </InputGroup>
-          </Col>
+  const raceGrid = activityList
+    .map((_activity, index) => {
+      return index % numOfColumns === 0
+        ? activityList.slice(index, index + numOfColumns)
+        : null;
+    })
+    .filter((activity) => activity)
+    .map((row, index) => {
+      return (
+        <Row key={index}>
+          {row.map((rowItem) => {
+            return (
+              <Col sm={4} style={{ padding: "16px" }} key={rowItem.id}>
+                <Activity
+                  name={rowItem.name}
+                  workoutType={rowItem.workout_type}
+                  startDate={rowItem.start_date}
+                  distance={calculate.metersToMiles(rowItem.distance, 2)}
+                  time={calculate.secondsToMinutes(rowItem.moving_time)}
+                  id={rowItem.id}
+                />
+              </Col>
+            );
+          })}
         </Row>
-        {this.state.isPageLoading ? <Loader /> : <div>{raceGrid}</div>}
-      </>
-    );
-  }
-}
+      );
+    });
+
+  return (
+    <>
+      <Row>
+        <Col sm={4} style={{ padding: "16px" }}>
+          <InputGroup>
+            <FormControl
+              placeholder="Search"
+              aria-label="Search"
+              aria-describedby="basic-addon2"
+              onChange={changeActivitySearchValue}
+              onKeyPress={(e) => {
+                if (e.key === "Enter") {
+                  searchActivities();
+                }
+              }}
+              value={searchCriteria}
+            />
+            <InputGroup.Append>
+              <Button variant="outline-secondary" onClick={searchActivities}>
+                Search
+              </Button>
+              <Button variant="outline-secondary" onClick={clearSearch}>
+                Clear
+              </Button>
+            </InputGroup.Append>
+          </InputGroup>
+        </Col>
+      </Row>
+      {pageLoading ? <Loader /> : <div>{raceGrid}</div>}
+    </>
+  );
+};
 
 export default Activities;
